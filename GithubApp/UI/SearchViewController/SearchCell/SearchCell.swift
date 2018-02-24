0b79f6d16c68699324123580f224ef976309743b
Marcin Chocilowicz <marcin.chocilowicz@gmail.com>
2018-02-24 15:02:57 +0100

    Setup SearchVC ui

---
+++ GithubApp/UI/SearchViewController/SearchCell/SearchCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCell.swift
+//  GithubApp
+//
+//  Created by Marcin on 24/02/2018.
+//  Copyright © 2018 Marcin. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell
+{
+
+}
--- GithubApp/UI/SearchViewController/SearchViewController.swift
+++ GithubApp/UI/SearchViewController/SearchViewController.swift
@@ -8,28 +8,53 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+// MARK: Publics
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+// MARK: View Lifecycle
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: Action handlers
+    
+    @IBAction func searchValueChanged(_ sender: Any)
+    {
+        
     }
-    */
-
 }
+
+// MARK: - UITableViewDataSource UITableViewDelegate
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate
+{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        
+    }
+}
+
